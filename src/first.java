6a8ee249b301061554a2d38e79a114ee8dd8bbf5
unique2k18 <the_quemal@mail.ru>
2017-11-26 19:51:02 +0200

    написал программу которая принимает кол-во элементов в массиве ar1.

---
+++ src/first.java
@@ -0,0 +1,7 @@
+public class first {
+    public static void main(String[] args) {
+        double [] ar1 = {1.15, 2.67, 64.5634, 6.735, 897};
+        System.out.println("Кол-во элементов в массиве ar1 - " + ar1.length);
+    }
+
+}
