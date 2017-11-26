a36827e0f409a52de2e35f9253005f5b31274eec
unique2k18 <the_quemal@mail.ru>
2017-11-26 19:59:20 +0200

    Написал программу, которая принимает все элемменты массива.

--- src/first.java
+++ src/first.java
@@ -2,6 +2,11 @@ public class first {
     public static void main(String[] args) {
         double [] ar1 = {1.15, 2.67, 64.5634, 6.735, 897};
         System.out.println("Кол-во элементов в массиве ar1 - " + ar1.length);
+        System.out.println("Элементы массива ar1 - " );
+        for (int i = 0; i<ar1.length; i++) {
+            System.out.println(ar1[i] + ", ");
+        }
+
     }
 
 }
